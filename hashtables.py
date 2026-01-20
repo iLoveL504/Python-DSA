b4b6f1df59f0d039e46393472fa9601b2ad7ac1f
Reyal <chingcuangco7904@gmail.com>
2026-01-20 21:36:22 +0800

    hashmaps

---
+++ hashtables.py
@@ -0,0 +1,27 @@
+# Hashtables / Hashmaps
+
+class HashTable:
+    def __init__(self):
+        self.MAX = 100
+        self.arr = [None for i in range(self.MAX)]
+
+    def get_hash(self, key):
+        h = 0
+        for char in key:
+            h += ord(char)
+        return h % self.MAX
+    
+    def __setitem__(self, key, val):
+        h = self.get_hash(key)
+        self.arr[h] = val
+
+    def __getitem__(self, key):
+        h = self.get_hash(key)
+        return self.arr[h]
+    
+t = HashTable()
+t['march 6'] = 130
+
+print(t['march 6'])
+
+# I just realized that hashmaps are essential the built it 'dicts' in python
