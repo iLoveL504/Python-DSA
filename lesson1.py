659ac35ce01dbc6b477c757daf352a2cbc975ef0
Reyal <chingcuangco7904@gmail.com>
2026-01-20 20:48:56 +0800

    Linked List

--- lesson1.py
+++ lesson1.py
@@ -1,23 +1,104 @@
-# Lesson1 - Binary Search, Linked Lists and Complexity
-class SinglyNode:
+# Linked Lists
 
-    def __init__(self, val, next=None):
-        self.val = val
+class Node:
+    def __init__ (self, data=None, next=None):
+        self.data = data
         self.next = next
+     
 
-    def __str__(self):
-        return str(self.val)
+class LinkedList:
+    def __init__ (self):
+        self.head = None
+
+    def insert_at_beginning(self, data):
+        node = Node(data, self.head)
+        self.head = node
+
+    def insert_at_end(self, data):
+        if self.head is None:
+            self.head = Node(data, None)
+            return
+        
+        current = self.head
+        while current.next:
+            current = current.next
+
+        current.next = Node(data, None)
+
+    def insert_values(self, data_list):
+        self.head = None
+        for data in data_list:
+            self.insert_at_end(data)
+
+    def get_length(self):
+        count = 0
+        current = self.head
+        while current:
+            count += 1
+            current = current.next
+
+        return count
     
-Head = SinglyNode(1)
-A = SinglyNode(3)
-B = SinglyNode(4)
-C = SinglyNode(7)  
+    def remove_at(self, index):
+        if index < 0 or index >=self.get_length():
+            raise Exception("Invalid index")
+        
+        if index == 0:
+            self.head = self.head.next
+            return
+        
+        count = 0
+        current = self.head
+        while current:
+            if count == index - 1:
+                current.next = current.next.next
+                break
+            current = current.next
+            count += 1
 
-Head.next = A
-A.next = B
-B.next = C
+    def insert_at(self, index, data):
+        if index<0 or index>=self.get_length():
+            raise Exception("Invalid index")
+        
+        if index==0:
+            self.insert_at_beginning(data)
+            return
+        
+        count = 0
+        current = self.head
+        while current:
+            print(current.data)   
+            if count == index:
+                node = Node(data, current.next)
+                print('current data: ', current.data)
+                print('node data: ', node.data)
+                print('current node data: ', current.data)
+                print('current next data: ', current.next.data)
+                current.next = node
 
-curr = Head
-while curr:
-    print(curr)
-    curr = curr.next
+                break
+            print('count: ', count)
+            current = current.next
+            count += 1
+
+    
+    def print(self):
+        if self.head is None:
+            print("list is empty")
+            return
+        
+        current = self.head
+        list = []
+
+        while current:
+            list.append(str(current.data))
+            current = current.next
+
+        print(' -> '.join(list))
+
+ll = LinkedList()
+ll.insert_values([54, 73, 14, 66])
+ll.remove_at(1)
+ll.insert_at(1, 73)
+ll.print()
+
